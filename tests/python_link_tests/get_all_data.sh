8e54c92b79360768b04609439b8d2af42c93b1d7
Nortamo <40563680+Nortamo@users.noreply.github.com>
2019-10-07 09:22:20 +0300

    Adds check for manual html anchors

--- tests/python_link_tests/get_all_data.sh
+++ tests/python_link_tests/get_all_data.sh
@@ -9,10 +9,14 @@ get_all_files(){
 }
 
 get_all_headders(){
-    
     grep -r -o -n "<h[0-9] id=.*\">" --include \*.html $1
-
 }
+
+get_all_anchors(){
+    grep -E -r -o -n "<a name=.*\">|<a id=.*\">" --include \*.html $1
+}
+
+
 
 get_all_nav_links(){
 cat $1  | grep "^\s*[^#]*\.md" | cut -d":" -f 2 | sed 's/^\s/docs\//g' | grep -v "#"
@@ -30,4 +34,6 @@ nav_list=$4
 get_all_internal_links "docs" > "$link_list"
 get_all_files "docs" > "$file_list"
 get_all_headders "site" > "$headder_list"
+get_all_anchors "site" >> "$headder_list"
 get_all_nav_links "mkdocs.yml" > "$nav_list"
+
