f46a18f63e147aa8edee8cb1af85afb3df576cb1
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-09-02 08:53:28 +0300

    Adds script to generate time stamps

---
+++ scripts/generate_stamps.sh
@@ -0,0 +1,8 @@
+
+time_stamps=$(git ls-files -z docs/ | xargs -0 -n1 -I{} -- git log -1 --format="%ad {}" {} | grep .md | cut -d " " -f 1,2,3,5,7)
+
+while read -r line; do
+    date=$(echo $line | cut -d " " -f 1-4 )
+    file_name=$(echo $line | cut -d " " -f 5)
+    echo -e "\n_Last edited ${date}_" >> "$file_name"
+done <<< "$time_stamps"
