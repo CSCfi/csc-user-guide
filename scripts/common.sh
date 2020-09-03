a47d6ef97fa92c66c5139ee96fe3d838f47fc068
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-09-03 15:51:35 +0300

    Remove extra file

--- scripts/common.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Scripts are run from the top level directory of the documentation so all paths
-# are relative to that.
-app_dir="docs/apps"
-ignores="--exclude=docs/apps/index.md --exclude=docs/apps/alpha.md --exclude=docs/apps/by_system.md"
