0bdd6932958ce2ab465a57844c0fc46a6898b0ff
Nortamo <40563680+Nortamo@users.noreply.github.com>
2020-09-09 08:28:41 +0300

    Removes chipster as a system

--- scripts/generate_by_system.sh
+++ scripts/generate_by_system.sh
@@ -6,15 +6,14 @@ generated_file="docs/apps/by_system.md"
 echo -e "# Applications by availability\n" > $generated_file
 
 # Case sensitive, the title for the system category
-system_name=("Mahti" "Puhti" "Chipster")
+system_name=("Mahti" "Puhti")
 # Not case sensitive, the keyword to grep to determine if a software is available on a system
 # For exceptions add an entry to skip_system.txt
 # Format: SKIP_[system_key] [filename]
-system_key=("mahti" "puhti" "chipster")
+system_key=("mahti" "puhti")
 
 system_desc=("CSC supercomputer for massively parallel jobs"\ 
-    "CSC supercomputer for small and medium jobs"\ 
-    "Open source platform for data analysis")
+    "CSC supercomputer for small and medium jobs") 
 
 
 num_systems=${#system_name[@]}
