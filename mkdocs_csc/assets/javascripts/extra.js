da32b1d0ceb5900bfd66e59fb1d3f29e862ab0c4
johannes <johannes.nyman@csc.fi>
2020-12-31 10:07:24 +0200

    Added IE compatibility

--- mkdocs_csc/assets/javascripts/extra.js
+++ mkdocs_csc/assets/javascripts/extra.js
@@ -1,7 +1,8 @@
 // This enables opening main navigation tabs with enter key
 var nav_links = document.getElementsByClassName("md-nav__link");
 var openNavIfEnterClicked = function (event) {
-  if (event.key === "Enter") {
+  var keyCode = event.which || event.keyCode;
+  if (keyCode === 13) {
     event.preventDefault();
     event.target.click();
   }
@@ -11,15 +12,15 @@ for (var i = 0; i < nav_links.length; i++) {
   nav_links[i].addEventListener("keyup", openNavIfEnterClicked, false);
 }
 
-// With Safari browser, tab key navigation gets stuck at search, this is hack to prevent it
+// With Safari/IE browser, tab key navigation gets stuck at search, this is hack to prevent it
 var search_bar = document.getElementsByClassName("md-search__input");
 var moveFocusFromSearch = function (event) {
-  if (event.key === "Tab") {
+  var keyCode = event.which || event.keyCode;
+  if (keyCode === 9) {
     var home_nav = document.querySelector('[title="Home"]');
     home_nav.focus();
   }
 };
-
 search_bar[0].addEventListener("keyup", moveFocusFromSearch, false);
 
 // Disable shortcut key s for search as it messes with assistive technology apparently
