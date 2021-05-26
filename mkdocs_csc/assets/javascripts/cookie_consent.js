6f7d95c682a1a3fccdc976748fea34a2e5d41646
Nortamo <40563680+Nortamo@users.noreply.github.com>
2021-05-26 16:14:28 +0300

    First mock of banner

--- mkdocs_csc/assets/javascripts/cookie_consent.js
+++ mkdocs_csc/assets/javascripts/cookie_consent.js
@@ -8,6 +8,15 @@ window.cookieconsent.initialise({
             "text": "#237afc"
         }
     },
+    "content": {
+    "message": "Our website uses cookies to ensure you get the best experience on our website. Our website may also include third party cookies. ",
+    "link": "Read more",
+    "href": "https://www.csc.fi/en/use-of-cookies",
+    "policy": 'Cookie Policy',
+    'allow': 'Approve',
+    'deny': 'Refuse'    
+    }
+    ,
     onStatusChange: function(status) {
         if(this.hasConsented()){
             cookieconsent.status = "allow";
