5f08ffe23f69461c8e5dc06dacee29cd3407b97a
Nortamo <40563680+Nortamo@users.noreply.github.com>
2021-06-14 09:43:40 +0300

    Generate new twice

--- scripts/generate_new.sh
+++ scripts/generate_new.sh
@@ -1,14 +1,14 @@
 t_dir="docs/support/wn"
-main_page_link="../../whats-new.md"
-main_page="docs/support/whats-new.md"
 
 
 cd $t_dir
 for filename in ./*; do
     title=$(grep "^# " $filename | sed 's/#//g')
+    echo -e "\n## $title\n" >> ../../index.md
     echo -e "\n## $title\n" >> ../whats-new.md
     topics=$(grep "^## " $filename | sed 's/#//g' | sed 's/ *$//g' | sed 's/^ *//g' )
     while IFS= read -r topic; do
-        echo "- [$topic](wn/$(echo $filename | sed 's@^./@@g')#$(python ../../../scripts/convert.py "$topic") )" >> ../whats-new.md 
+        echo "- [$topic](wn/$(echo $filename | sed 's@^./@@g')#$(python ../../../scripts/convert.py "$topic") )" >> ../whats-new.md
+        echo "- [$topic](support/wn/$(echo $filename | sed 's@^./@@g')#$(python ../../../scripts/convert.py "$topic") )" >> ../../index.md
     done <<< "$topics"     
 done
