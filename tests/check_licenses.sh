cb3dcf52c3ba244612c6df2118ff8abdd713cb67
Joonas Somero <joonas.somero@csc.fi>
2022-05-18 09:49:02 +0300

    a bit more sophisticated regex for license check

--- tests/check_licenses.sh
+++ tests/check_licenses.sh
@@ -1,8 +1,16 @@
 file_type="*.md"
+regex="^#\{2,3\} Licenses\?"
 directory="docs/apps"
-regex="## License"
 
-APPS_WITHOUT_LICENSE=$(grep -rL --include "$file_type" --exclude index.md -- "$regex" $directory)
+
+APPS_WITHOUT_LICENSE=$(grep -r -L \
+    --include "$file_type" \
+    --exclude index.md \
+    --exclude alpha.md \
+    --exclude by_system.md \
+    -- \
+    "$regex" $directory)
+
 
 if [ -n "$APPS_WITHOUT_LICENSE" ]; then
     echo "Found application(s) with no entry for license:"
