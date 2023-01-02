2e3948006863206a698261bf27338a2b362c82fa
JMuff22 <jake.muff@csc.fi>
2023-01-02 13:51:24 +0200

    Update check_index.sh

--- tests/check_index.sh
+++ tests/check_index.sh
@@ -4,11 +4,11 @@
 
 # find ./docs -type d '!' -exec test -e "{}/index.md" ';' -print |grep -v 'img\|css' | tee >(wc -l)
 
-if [[ $(find ./docs -type d '!' -exec test -e "{}/index.md" ';' -print |grep -v 'img\|css\|images') ]]; then
+if [[ $(find ./docs -type d '!' -exec test -e "{}/index.md" ';' -print |grep -v 'img\|css\|images\|wn') ]]; then
     echo "There are directories without index.md"
-    find ./docs -type d '!' -exec test -e "{}/index.md" ';' -print |grep -v 'img\|css\|images'
-    exit 0
+    find ./docs -type d '!' -exec test -e "{}/index.md" ';' -print |grep -v 'img\|css\|images\|wn'
+    exit 1
 else
     echo "All directories contain index.md"
-    exit 1
+    exit 0
 fi
