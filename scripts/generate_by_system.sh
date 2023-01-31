2a900d820069968f6377cd65866e59cc148a66e8
Rasmus Kronberg <43936697+rkronberg@users.noreply.github.com>
2023-01-31 15:45:23 +0200

    punctuation

--- scripts/generate_by_system.sh
+++ scripts/generate_by_system.sh
@@ -39,7 +39,7 @@ do
     software=$(grep -f <(echo "$files") $app_dir/alpha.md)
     echo -e "\n## ${system_name[$i]} \n" >> $generated_file
     if [[ "${system_name[$i]}" == "LUMI" ]]; then
-        echo -e "!!! info \"Note\"\n    This list refers only to applications pre-installed by CSC under \`/appl/local/csc\`.\n    For a comprehensive list of available EasyBuild recipes, see the [LUMI Software\n    Library](https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/)\n" >> $generated_file
+        echo -e "!!! info \"Note\"\n    This list refers only to applications pre-installed by CSC under \`/appl/local/csc\`.\n    For a comprehensive list of available EasyBuild recipes, see the [LUMI Software\n    Library](https://lumi-supercomputer.github.io/LUMI-EasyBuild-docs/).\n" >> $generated_file
     fi
     echo "$software" >> $generated_file
 done
