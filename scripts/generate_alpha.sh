7d100bb0dae1eaf34d4972f92f339eeb15abbc6d
Joonas Somero <joonas.somero@csc.fi>
2023-02-24 10:52:11 +0200

    Added a newline

--- scripts/generate_alpha.sh
+++ scripts/generate_alpha.sh
@@ -8,5 +8,5 @@ echo -e "- [By discipline](by_discipline.md)" >> $generated_file
 echo -e "- [By availability](by_system.md)" >> $generated_file
 echo -e "- [By license](by_license.md)" >> $generated_file
 
-echo -e "## Applications in alphabetical order\n" >> $generated_file
+echo -e "\n## Applications in alphabetical order\n" >> $generated_file
 grep \* $app_dir/by_discipline.md | sort -f | uniq >> $generated_file
