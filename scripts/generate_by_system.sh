a1ac0da45917c046bcb523bb1798495c79372b66
Joonas Somero <joonas.somero@csc.fi>
2023-02-24 10:57:21 +0200

    Fixed blacklisting

--- scripts/generate_by_system.sh
+++ scripts/generate_by_system.sh
@@ -33,7 +33,7 @@ done
 for i in $( seq 0 $(($num_systems-1)) )
 do
     ignores=$(cat <(grep -i SKIP_ALL $ignore_file ) <(grep -i SKIP_${system_key[$i]} $ignore_file ) | awk  '{ print $2 }')
-    files=$(grep --exclude-from=<(echo "$ignores") -wi ${system_key[$i]} $app_dir/*.md | awk -F ":" '{print $1}' | sort | uniq | sed s%docs/apps/%%)
+    files=$(grep --exclude-from=<(echo "$ignores") -wirl ${system_key[$i]} $app_dir | sort | uniq | sed s%docs/apps/%%)
     software=$(grep -f <(echo "$files") $app_dir/index.md)
     echo -e "\n## ${system_name[$i]} \n" >> $generated_file
     if [[ "${system_name[$i]}" == "LUMI" ]]; then
