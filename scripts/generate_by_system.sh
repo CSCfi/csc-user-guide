ab96302398b6ee87a8ebe5a5902565174e0f9de7
Joonas Somero <joonas.somero@csc.fi>
2023-06-14 16:57:39 +0300

    List web interfaces separately
    Additionally, some refactoring

--- scripts/generate_by_system.sh
+++ scripts/generate_by_system.sh
@@ -13,10 +13,14 @@ echo -e "# Applications by availability\n" > $generated_file
 
 # Case sensitive, the title for the system category
 system_name=("Mahti" "Puhti" "LUMI" "Mahti web interface" "Puhti web interface")
+
+# Prefix to indicate a keyword for a web interface
+interactive_prefix="www"
+
 # Not case sensitive, the keyword to grep to determine if a software is available on a system
 # For exceptions add an entry to skip_system.txt
 # Format: SKIP_[system_key] [filename]
-system_key=("mahti" "www-mahti" "puhti" "www-puhti" "lumi")
+system_key=("mahti" "puhti" "lumi" "$interactive_prefix-mahti" "$interactive_prefix-puhti")
 
 system_desc=("CSC supercomputer for massively parallel jobs"\
     "CSC supercomputer for small and medium jobs"\
@@ -24,31 +28,46 @@ system_desc=("CSC supercomputer for massively parallel jobs"\
     "Web interface for Mahti"\
     "Web interface for Puhti")
 
+system_prefix () { echo ${system_key[$1]%%-*}; }
+system_anchor () { lower_case=${system_name[$1],,}; echo "#${lower_case//\ /-}"; }
 
 num_systems=${#system_name[@]}
 num_keys=${#system_key[@]}
 
-if [[ "$num_keys" -ne "$num_systems" ]]; then
+if [[ "$num_keys" -ne "$num_systems" ]]
+then
     echo "Number of search keys must match the number of systems"
     exit 1;
 fi
 
-echo -e "Applications available on\n" >> $generated_file
+indexes=$( seq 0 $(($num_systems-1)) )
 
-for i in $( seq 0 $(($num_systems-1)) )
+echo -e "Applications available on\n" >> $generated_file
+for i in $indexes
 do
-    system_name_lower_case=${system_name[$i],,}
-    system_anchor="#${system_name_lower_case//\ /-}"
-    echo "- [${system_name[$i]}](by_system.md$system_anchor), ${system_desc[$i]}" >> $generated_file
+    if [[ $(system_prefix $i) != $interactive_prefix ]]
+    then
+        echo "- [${system_name[$i]}]($( system_anchor $i )), ${system_desc[$i]}" >> $generated_file
+    fi
 done
 
-for i in $( seq 0 $(($num_systems-1)) )
+echo -e "\nInteractive web applications available on\n" >> $generated_file
+for i in $indexes
+do
+    if [[ $(system_prefix $i) == $interactive_prefix ]]
+    then
+        echo "- [${system_name[$i]}]($( system_anchor $i ))" >> $generated_file
+    fi
+done
+
+for i in $indexes
 do
     ignores=$(cat <(grep -i SKIP_ALL $ignore_file ) <(grep -i SKIP_${system_key[$i]} $ignore_file ) | awk  '{ print $2 }')
     files=$(grep --exclude-from=<(echo "$ignores") -wirl ${system_key[$i]} $app_dir | sort | uniq | sed s%docs/apps/%%)
     software=$(grep -f <(echo "$files") $app_dir/index.md)
     echo -e "\n## ${system_name[$i]} \n" >> $generated_file
-    if [[ "${system_name[$i]}" == "LUMI" ]]; then
+    if [[ "${system_name[$i]}" == "LUMI" ]]
+    then
         echo -e "!!! info \"Note\"" >> $generated_file
         echo -e "    This list refers only to applications pre-installed by CSC under \`/appl/local/csc\`." >> $generated_file
         echo -e "    For a comprehensive list of available EasyBuild recipes, see the [LUMI Software" >> $generated_file
