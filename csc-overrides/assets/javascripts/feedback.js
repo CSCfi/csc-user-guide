34096cc3230e3234dd41ceb5390da4a9d0d8f159
Joonas Somero <joonas.somero@csc.fi>
2023-09-18 17:00:33 +0300

    Feedback widget continued

--- csc-overrides/assets/javascripts/feedback.js
+++ csc-overrides/assets/javascripts/feedback.js
@@ -1,9 +1,62 @@
-const handleSubmit = rating => {
-  console.log(rating)
+const feedbackForm = document.forms.feedback
+const ratingKey = `${window.location.pathname} rating`
+const pageEdited = Date.parse(feedbackForm.dataset.sourceEdited)
+
+const ratingExists = () => {
+  const item = window.localStorage.getItem(ratingKey)
+  if (!item) return false
+
+  const rating = JSON.parse(item)
+  return rating.date > pageEdited
 }
 
-const feedbackFormElement = document.forms.feedback
-feedbackFormElement.addEventListener("submit", event => {
+const getRating = () => {
+  const item = window.localStorage.getItem(ratingKey)
+
+  return JSON.parse(item).rating
+}
+
+const setRating = rating => {
+  const item = JSON.stringify({ date: Date.now(), rating })
+
+  window.localStorage.setItem(ratingKey, item)
+  setForm()
+}
+
+const trackRating = rating => {
+  const [ first, ...rest ] = rating
+  const name = first.toUpperCase() + rest.join("")
+  const value = rating === "positive" ? 1 : -1
+  const event = [ "trackEvent", "Feedback", "Feedback Button Click", name, value ]
+
+  _paq && _paq.push(event)
+}
+
+const handleSubmit = rating => {
+  if (!ratingExists()) {
+    setRating(rating)
+    trackRating(rating)
+  }
+}
+
+const setForm = () => {
+  if (ratingExists()) {
+    const rating = getRating()
+
+    const feedbackFormElement = document.querySelector(".csc-feedback")
+    feedbackFormElement.classList.add("csc-submitted")
+
+    const feedbackFormContentElement = document.querySelector(".csc-feedback-content")
+    feedbackFormContentElement.classList.add("csc-rated", `csc-rated--${rating}`)
+  }
+}
+
+feedbackForm.addEventListener("submit", event => {
   event.preventDefault()
-  handleSubmit(event.submitter.value)
+
+  const { submitter: { value: rating } } = event
+
+  handleSubmit(rating)
 })
+
+window.addEventListener("load", setForm)
