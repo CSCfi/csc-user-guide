770729889a49f0ac236cd33e5c9fa91432172f3f
Joonas Somero <joonas.somero@csc.fi>
2025-03-19 16:03:22 +0200

    Fix live reload for serve

--- development/entrypoint.bash
+++ development/entrypoint.bash
@@ -1,21 +1,13 @@
-#! /usr/bin/bash
+#! /usr/bin/env bash
 
 set -e
 
 declare -gr mkdocs_BIN='/usr/local/bin/mkdocs' \
-            mkdocs_BASE_CONFIG='mkdocs.yml' \
             mkdocs_IP='0.0.0.0' \
             mkdocs_PORT='8000'
 
-get_config() {
-  cat << EOF
-INHERIT: ${mkdocs_BASE_CONFIG}
-dev_addr: ${mkdocs_IP}:${mkdocs_PORT}
-EOF
-}
-
 mkdocs_serve() {
-  get_config | $mkdocs_BIN 'serve' --config-file - "$@"
+  $mkdocs_BIN 'serve' --dev-addr=${mkdocs_IP}:${mkdocs_PORT} "$@"
 }
 
 case $1 in
